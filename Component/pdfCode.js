f12b46144f033693b3b1155c18b00fcd267066c3
AkhandPratapSingh1 <akhandpratap6397@gmail.com>
2023-11-20 17:45:23 +0530

    first build

--- Component/pdfCode.js
+++ Component/pdfCode.js
@@ -23,149 +23,228 @@ const PdfCode = (
   PaymentType,
   RemainingBalance
 ) => `
-<html>
+<!DOCTYPE html>
+<html lang="en">
   <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no" />
-  </head>
-  <body >
-    <div style="min-height: auto;
-    width: 100%;
-    height : 97vh;
-    border: solid 2px #000;"  >
-    <div style="height: auto;
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    /* padding: 20px; */
-    justify-content: space-between;
-    align-items: center;">
-    <div class="data-title">
-        <div style="display: flex;
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+    <title>Akhand Pratap Invoice App</title>
+    <style>
+      body {
+        font-family: 'Arial', sans-serif;
+        color: #333;
+        background-color: #f0f0f0;
+        margin: 0;
+        padding: 0;
+      }
+
+      .container {
+        min-height: auto;
+        width: 100%;
+        height: 97vh;
+        border: 2px solid #000;
+        background-color: #fff;
+      }
+
+      .header {
+        height: auto;
+        width: 100%;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+        align-items: center;
+        background-color: #333;
+        color: #fff;
+      }
+
+      .data-title {
+        display: flex;
         flex-direction: column;
         align-items: flex-start;
-        font-size: 2rem;  
-        padding-left: 20px;">Akhand Pratap Invoice App<br></div>
-    <div style="
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start; 
-    padding-left: 20px;
-    ">Demo Invoice App- A React Native Android App.</div>
-    </div>
-   
-        <img style="
+        font-size: 2rem;
+        padding-left: 20px;
+        color: #fff;
+      }
+
+      .subtitle {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding-left: 20px;
+        color: #ccc;
+      }
+
+      .logo {
         height: 90px;
-    width: 90px;
-    margin-right:15px;
-        " src="https://i.ibb.co/nLdT74K/Tech.png" />
-    </div>
-    <hr />
-        <hr/>
+        width: 90px;
+        margin-right: 15px;
+      }
 
+      hr {
+        border-color: #ccc;
+      }
 
-        <div style="
+      .info-container {
         width: 100%;
         height: auto;
         padding: 15px;
         display: flex;
         flex-direction: row;
         justify-content: space-evenly;
-        ">
-            <div style="
-            width: 50%;
-            align-items: flex-start;
-            ">
-                <p class="invoice-user">
-                    Bill To : <br/>
-                    Name : ${name} <br/>
-                    Address : ${Address} <br/>
-                    Phone No : +91 ${Mobile_No}
-                </p>
-            </div>
-            <div style="align-items: flex-end;">
-                <p>Invoice No : ${Invoice}<br/>
-                Date : ${dateFormat(Date.now(), "dd-mm-yyyy")}<br/>
-                Time :${GetTime(new Date())}</p>
-                <br/>
-                <br/>
-                <p>Mobile No :- <br/>
-                +91 6397421539<br/>
-                +91 9759401072
-                </p>
-            </div>
-        </div>
-        <hr/>
-        <hr/>
-        <div style="height: auto;
+      }
+
+      .info-section {
+        width: 50%;
+        align-items: flex-start;
+      }
+
+      .invoice-user {
+        color: #333;
+      }
+
+      .invoice-details {
+        align-items: flex-end;
+      }
+
+      .invoice-details p {
+        color: #333;
+      }
+
+      .table-container {
+        height: auto;
         width: 100%;
         display: flex;
         flex-direction: column;
-        align-items: center;">
-            <table style="width:100%; border-collapse: collapse;">
-                <tr style="background-color: rgba(255, 0, 62, 0.8); color: white;">
-                  <th style="height: 30px;">Index</th>
-                  <th style="height: 30px;">Product Name</th>
-                  <th style="height: 30px;">Price(Per)</th>
-                  <th style="height: 30px;">Bras</th>
-                  <th style="height: 30px;">Total</th>
-                </tr>
-                <tr style="background-color: rgba(246, 221, 178, 0.8);">
-                  <td style="text-align: center;height: 30px;">1</td>
-                  <td style="text-align: center;height: 30px;">${Product}</td>
-                  <td style="text-align: center;height: 30px;">${parseFloat(
-                    parseFloat(Total) / parseFloat(Quantity)
-                  ).toFixed(2)}</td>
-                  <td style="text-align: center;height: 30px;">${Quantity}</td>
-                  <td style="text-align: center;height: 30px;">₹ ${Total}</td>
-                </tr>
-               
-              </table>
-              
-                <!-- <div style="align-self: flex-end;margin-right: 10px;font-style: bold;">Received balance :  1</div>
-          
-              <hr/>
-              <div style="align-self: flex-end;margin-right: 10px;font-style: bold;">Grand Total : 1</div>
-              <hr/>
-              <div style="align-self: flex-end;margin-right: 10px;font-style: bold;">Payment Mode : Cash</div>
-              <hr/> -->
-              <div style="width:100%;align-self: flex-end; display: flex; flex-direction: row;">
-                <div style="width:40%"></div>
-                  <table style="width: 50%; align-self: flex-end;">
-                  <tr>
-                  <th style="text-align: start;">Grand Total : </th>
-                  <td style="text-align: center;height: 30px;">₹ ${Total}</td>
-              </tr>
-                        <tr style="border-bottom: solid ;">
-                            <th style="text-align: start;">Received Balance : </th>
-                            <td style="text-align: center;height: 30px;">₹ ${ReceivedBalance}</td>
-                        </tr>
-                       
-                        <tr style="border-bottom: solid ;">
-                        <th style="text-align: start;">Remaining Balance : </th>
-                        <td style="text-align: center;height: 30px;">₹ ${RemainingBalance}</td>
-                    </tr>
-                        <tr>
-                            <th style="text-align: start;">Payment Method: </th>
-                            <td style="text-align: center;height: 30px;">${PaymentType}</td>
-                        </tr>
-                  </table>
-              </div>
-        </div>
-        <hr/>
-        <hr/>
-        <div style="height:auto; padding: 20px;">
+        align-items: center;
+      }
 
-            <p>Account Details - <br/>
-            Bank Name: DEMO BANK, AGRA<br/>
-            Bank Account no : 123456789<br/>
-            Bank IFSC code : DEMO0002411<br/>
-            </p>
+      table {
+        width: 100%;
+        border-collapse: collapse;
+      }
 
+      th, td {
+        border: 1px solid #ccc;
+        padding: 8px;
+        text-align: center;
+      }
+
+      th {
+        background-color: #ff003e;
+        color: #fff;
+      }
+
+      .total-section {
+        width: 100%;
+        align-self: flex-end;
+        display: flex;
+        flex-direction: row;
+      }
+
+      .total-label {
+        width: 40%;
+      }
+
+      .account-details {
+        height: auto;
+        padding: 20px;
+        color: #333;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <div class="header">
+        <div class="data-title">
+          Akhand Pratap Invoice App
         </div>
+        <div class="subtitle">
+          Demo Invoice App - A React Native Android App.
+        </div>
+        <img class="logo" src="https://i.ibb.co/nLdT74K/Tech.png" />
+      </div>
+      <hr />
+      <hr />
+
+      <div class="info-container">
+        <div class="info-section">
+          <p class="invoice-user">
+            Bill To: <br/>
+            Name: ${name} <br/>
+            Address: ${Address} <br/>
+            Phone No: +91 ${Mobile_No}
+          </p>
+        </div>
+        <div class="info-section invoice-details">
+          <p>Invoice No: ${Invoice}<br/>
+          Date: ${dateFormat(Date.now(), "dd-mm-yyyy")}<br/>
+          Time: ${GetTime(new Date())}</p>
+          <br/>
+          <br/>
+          <p>Mobile No: <br/>
+          +91 6397421539<br/>
+          +91 9759401072
+          </p>
+        </div>
+      </div>
+      <hr/>
+      <hr/>
+
+      <div class="table-container">
+        <table>
+          <tr>
+            <th>Index</th>
+            <th>Product Name</th>
+            <th>Price(Per)</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+          <tr>
+            <td>1</td>
+            <td>${Product}</td>
+            <td>${parseFloat(parseFloat(Total) / parseFloat(Quantity)).toFixed(2)}</td>
+            <td>${Quantity}</td>
+            <td>₹ ${Total}</td>
+          </tr>
+        </table>
+
+        <div class="total-section">
+          <div class="total-label"></div>
+          <table>
+            <tr>
+              <th>Grand Total:</th>
+              <td>₹ ${Total}</td>
+            </tr>
+            <tr>
+              <th>Received Balance:</th>
+              <td>₹ ${ReceivedBalance}</td>
+            </tr>
+            <tr>
+              <th>Remaining Balance:</th>
+              <td>₹ ${RemainingBalance}</td>
+            </tr>
+            <tr>
+              <th>Payment Method:</th>
+              <td>${PaymentType}</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <hr/>
+      <hr/>
+
+      <div class="account-details">
+        <p>Account Details - <br/>
+        Bank Name: DEMO BANK, AGRA<br/>
+        Bank Account no: 123456789<br/>
+        Bank IFSC code: DEMO0002411</p>
+      </div>
 
     </div>
   </body>
 </html>
+
+
 `;
 
 const style = `
